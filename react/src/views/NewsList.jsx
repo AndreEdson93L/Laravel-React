27871307433a65bea8948dbb062cc04bfb926b22
Andrea Edson Lorenzoni <andrea@gmail.com>
2023-04-14 22:40:59 +0100

    Fix aside bar (nav bar).

--- react/src/views/NewsList.jsx
+++ react/src/views/NewsList.jsx
@@ -1,28 +1,43 @@
 // components/NewsList.jsx
 
-import React, { useEffect, useState } from 'react';
-import axios from '../axios-client'; // Make sure to import your axios instance
+import React, { useEffect, useState } from "react";
+import axios from "../axios-client"; // Make sure to import your axios instance
 
 const NewsList = () => {
   const [news, setNews] = useState([]);
 
+  const [search, setSearch] = useState("");
+  const [filters, setFilters] = useState({
+    date: "",
+    category: "",
+    source: "",
+  });
+  const [preferences, setPreferences] = useState({
+    sources: [],
+    categories: [],
+    authors: [],
+  });
+
   useEffect(() => {
     const fetchNews = async () => {
-        try {
-          const response = await axios.get('https://newsapi.org/v2/top-headlines', {
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines",
+          {
             params: {
-              country: 'de',
-              category: 'business',
+              country: "de",
+              category: "business",
               //apiKey: process.env.REACT_APP_NEWS_API_KEY,
-              apiKey: '00d3b42d7c444ec9bd4f6577e4aa6b59',
+              apiKey: "00d3b42d7c444ec9bd4f6577e4aa6b59",
               // Include additional parameters like search, filters, and preferences
             },
-          });
-          setNews(response.data.articles);
-        } catch (error) {
-          console.error('Error fetching news:', error);
-        }
-      };
+          }
+        );
+        setNews(response.data.articles);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    };
 
     fetchNews();
   }, []);
