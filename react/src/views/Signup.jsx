3b53886c1ab5a5bf194fcd60c5b6bfd3fd3d3730
Andrea Edson Lorenzoni <andrea@gmail.com>
2023-04-17 18:57:58 +0100

    Add bootstrap to Signup and UserForm.

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import axiosClient from "../axios-client.js";
 import { createRef, useState } from "react";
 import { useStateContext } from "../context/ContextProvider.jsx";
-import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button, Alert } from "react-bootstrap";
 
 export default function Signup() {
   const nameRef = createRef();
@@ -39,50 +39,38 @@ export default function Signup() {
     <Container className="my-4">
       <Row className="justify-content-center">
         <Col md={6}>
-          <h1 className="text-center">Signup for Free</h1>
-          <Form onSubmit={onSubmit}>
-            {errors && (
-              <Alert variant="danger">
-                {Object.keys(errors).map((key) => (
-                  <p key={key}>{errors[key][0]}</p>
-                ))}
-              </Alert>
-            )}
-            <Form.Group controlId="name">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control ref={nameRef} type="text" placeholder="Full Name" />
-            </Form.Group>
-            <Form.Group controlId="email">
-              <Form.Label>Email Address</Form.Label>
-              <Form.Control
-                ref={emailRef}
-                type="email"
-                placeholder="Email Address"
-              />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                ref={passwordRef}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Group>
-            <Form.Group controlId="password_confirmation">
-              <Form.Label>Repeat Password</Form.Label>
-              <Form.Control
-                ref={passwordConfirmationRef}
-                type="password"
-                placeholder="Repeat Password"
-              />
-            </Form.Group>
-            <Button className="btn btn-block" type="submit">
-              Signup
-            </Button>
-            <p className="message text-center">
-              Already registered? <Link to="/login">Sign In</Link>
-            </p>
-          </Form>
+          <h1 className="text-center mb-3">Signup for Free</h1>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={onSubmit}>
+                {errors && (
+                  <Alert variant="danger" className="mb-3">
+                    {Object.keys(errors).map((key) => (
+                      <p key={key}>{errors[key][0]}</p>
+                    ))}
+                  </Alert>
+                )}
+                <Form.Group controlId="name" className="mb-3">
+                  <Form.Control ref={nameRef} type="text" placeholder="Full Name" />
+                </Form.Group>
+                <Form.Group controlId="email" className="mb-3">
+                  <Form.Control ref={emailRef} type="email" placeholder="Email Address" />
+                </Form.Group>
+                <Form.Group controlId="password" className="mb-3">
+                  <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="password_confirmation" className="mb-3">
+                  <Form.Control ref={passwordConfirmationRef} type="password" placeholder="Repeat Password" />
+                </Form.Group>
+                <Button className="w-100 mb-3" type="submit">
+                  Signup
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <p className="text-center mt-3">
+            Already registered? <Link to="/login">Sign In</Link>
+          </p>
         </Col>
       </Row>
     </Container>
--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -2,7 +2,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axiosClient from "../axios-client.js";
 import { useStateContext } from "../context/ContextProvider.jsx";
-import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button, Alert } from "react-bootstrap";
 
 export default function UserForm() {
   const navigate = useNavigate();
@@ -68,61 +68,60 @@ export default function UserForm() {
     <Container className="my-4">
       <Row className="justify-content-center">
         <Col md={6}>
-          {user.id && <h1>Update User: {user.name}</h1>}
-          {!user.id && <h1>New User</h1>}
-          {loading && <div className="text-center">Loading...</div>}
-          {errors && (
-            <Alert variant="danger">
-              {Object.keys(errors).map((key) => (
-                <p key={key}>{errors[key][0]}</p>
-              ))}
-            </Alert>
-          )}
-          {!loading && (
-            <Form onSubmit={onSubmit}>
-              <Form.Group controlId="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  value={user.name}
-                  onChange={(ev) => setUser({ ...user, name: ev.target.value })}
-                  placeholder="Name"
-                />
-              </Form.Group>
-              <Form.Group controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  value={user.email}
-                  onChange={(ev) =>
-                    setUser({ ...user, email: ev.target.value })
-                  }
-                  placeholder="Email"
-                />
-              </Form.Group>
-              <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(ev) =>
-                    setUser({ ...user, password: ev.target.value })
-                  }
-                  placeholder="Password"
-                />
-              </Form.Group>
-              <Form.Group controlId="password_confirmation">
-                <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(ev) =>
-                    setUser({ ...user, password_confirmation: ev.target.value })
-                  }
-                  placeholder="Password confirmation"
-                />
-              </Form.Group>
-              <Button className="btn" type="submit">
-                Save
-              </Button>
-            </Form>
-          )}
+          <h1 className="text-center mb-3">{user.id ? `Update User: ${user.name}` : 'New User'}</h1>
+          <Card>
+            <Card.Body>
+              {loading && <div className="text-center">Loading...</div>}
+              {errors && (
+                <Alert variant="danger" className="mb-3">
+                  {Object.keys(errors).map((key) => (
+                    <p key={key}>{errors[key][0]}</p>
+                  ))}
+                </Alert>
+              )}
+              {!loading && (
+                <Form onSubmit={onSubmit}>
+                  <Form.Group controlId="name" className="mb-3">
+                    <Form.Control
+                      value={user.name}
+                      onChange={(ev) => setUser({ ...user, name: ev.target.value })}
+                      placeholder="Name"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="email" className="mb-3">
+                    <Form.Control
+                      value={user.email}
+                      onChange={(ev) =>
+                        setUser({ ...user, email: ev.target.value })
+                      }
+                      placeholder="Email"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="password" className="mb-3">
+                    <Form.Control
+                      type="password"
+                      onChange={(ev) =>
+                        setUser({ ...user, password: ev.target.value })
+                      }
+                      placeholder="Password"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="password_confirmation" className="mb-3">
+                    <Form.Control
+                      type="password"
+                      onChange={(ev) =>
+                        setUser({ ...user, password_confirmation: ev.target.value })
+                      }
+                      placeholder="Password confirmation"
+                    />
+                  </Form.Group>
+                  <Button className="w-100 mb-3" type="submit">
+                    Save
+                  </Button>
+                </Form>
+              )}
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
