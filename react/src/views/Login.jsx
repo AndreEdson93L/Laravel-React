9bc3785bc1039d2692ad6cc030b9a4bae86ffcf2
Andrea Edson Lorenzoni <andrea@gmail.com>
2023-04-17 18:47:20 +0100

    Bootstrap Login view.

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,53 +1,86 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import axiosClient from "../axios-client.js";
-import {createRef} from "react";
-import {useStateContext} from "../context/ContextProvider.jsx";
+import { createRef } from "react";
+import { useStateContext } from "../context/ContextProvider.jsx";
 import { useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Alert,
+} from "react-bootstrap";
 
 export default function Login() {
-  const emailRef = createRef()
-  const passwordRef = createRef()
-  const { setUser, setToken } = useStateContext()
-  const [message, setMessage] = useState(null)
+  const emailRef = createRef();
+  const passwordRef = createRef();
+  const { setUser, setToken } = useStateContext();
+  const [message, setMessage] = useState(null);
 
-  const onSubmit = ev => {
-    ev.preventDefault()
+  const onSubmit = (ev) => {
+    ev.preventDefault();
 
     const payload = {
       email: emailRef.current.value,
       password: passwordRef.current.value,
-    }
-    axiosClient.post('/login', payload)
-      .then(({data}) => {
-        setUser(data.user)
+    };
+    axiosClient
+      .post("/login", payload)
+      .then(({ data }) => {
+        setUser(data.user);
         setToken(data.token);
       })
       .catch((err) => {
         const response = err.response;
         if (response && response.status === 422) {
-          setMessage(response.data.message)
+          setMessage(response.data.message);
         }
-      })
-  }
+      });
+  };
 
   return (
-    <div className="login-signup-form animated fadeInDown">
-      <div className="form">
-        <form onSubmit={onSubmit}>
-          <h1 className="title">Login into your account</h1>
-
-          {message &&
-            <div className="alert">
-              <p>{message}</p>
-            </div>
-          }
-
-          <input ref={emailRef} type="email" placeholder="Email"/>
-          <input ref={passwordRef} type="password" placeholder="Password"/>
-          <button className="btn btn-block">Login</button>
-          <p className="message">Not registered? <Link to="/signup">Create an account</Link></p>
-        </form>
-      </div>
-    </div>
-  )
+    <Container className="my-4">
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <h1 className="text-center mb-3">App News</h1>
+          <p className="text-center mb-4">
+            Search your favorite articles in science, technology, and more.
+          </p>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={onSubmit}>
+                {message && (
+                  <Alert variant="danger" className="mb-3">
+                    <p>{message}</p>
+                  </Alert>
+                )}
+                <Form.Group controlId="email" className="mb-3">
+                  <Form.Control
+                    ref={emailRef}
+                    type="email"
+                    placeholder="Email or phone number"
+                  />
+                </Form.Group>
+                <Form.Group controlId="password" className="mb-3">
+                  <Form.Control
+                    ref={passwordRef}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Button className="w-100 mb-3" type="submit">
+                  Login
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <p className="text-center mt-3">
+            Not registered? <Link to="/signup">Create an account</Link>
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,58 +1,90 @@
-import {Link} from "react-router-dom";
-import {createRef, useState} from "react";
+import { Link } from "react-router-dom";
 import axiosClient from "../axios-client.js";
-import {useStateContext} from "../context/ContextProvider.jsx";
+import { createRef, useState } from "react";
+import { useStateContext } from "../context/ContextProvider.jsx";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 
 export default function Signup() {
-  const nameRef = createRef()
-  const emailRef = createRef()
-  const passwordRef = createRef()
-  const passwordConfirmationRef = createRef()
-  const {setUser, setToken} = useStateContext()
-  const [errors, setErrors] = useState(null)
+  const nameRef = createRef();
+  const emailRef = createRef();
+  const passwordRef = createRef();
+  const passwordConfirmationRef = createRef();
+  const { setUser, setToken } = useStateContext();
+  const [errors, setErrors] = useState(null);
 
-  const onSubmit = ev => {
-    ev.preventDefault()
+  const onSubmit = (ev) => {
+    ev.preventDefault();
 
     const payload = {
       name: nameRef.current.value,
       email: emailRef.current.value,
       password: passwordRef.current.value,
       password_confirmation: passwordConfirmationRef.current.value,
-    }
-    axiosClient.post('/signup', payload)
-      .then(({data}) => {
-        setUser(data.user)
+    };
+    axiosClient
+      .post("/signup", payload)
+      .then(({ data }) => {
+        setUser(data.user);
         setToken(data.token);
       })
-      .catch(err => {
+      .catch((err) => {
         const response = err.response;
         if (response && response.status === 422) {
-          setErrors(response.data.errors)
+          setErrors(response.data.errors);
         }
-      })
-  }
+      });
+  };
 
   return (
-    <div className="login-signup-form animated fadeInDown">
-      <div className="form">
-        <form onSubmit={onSubmit}>
-          <h1 className="title">Signup for Free</h1>
-          {errors &&
-            <div className="alert">
-              {Object.keys(errors).map(key => (
-                <p key={key}>{errors[key][0]}</p>
-              ))}
-            </div>
-          }
-          <input ref={nameRef} type="text" placeholder="Full Name"/>
-          <input ref={emailRef} type="email" placeholder="Email Address"/>
-          <input ref={passwordRef} type="password" placeholder="Password"/>
-          <input ref={passwordConfirmationRef} type="password" placeholder="Repeat Password"/>
-          <button className="btn btn-block">Signup</button>
-          <p className="message">Already registered? <Link to="/login">Sign In</Link></p>
-        </form>
-      </div>
-    </div>
-  )
+    <Container className="my-4">
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <h1 className="text-center">Signup for Free</h1>
+          <Form onSubmit={onSubmit}>
+            {errors && (
+              <Alert variant="danger">
+                {Object.keys(errors).map((key) => (
+                  <p key={key}>{errors[key][0]}</p>
+                ))}
+              </Alert>
+            )}
+            <Form.Group controlId="name">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control ref={nameRef} type="text" placeholder="Full Name" />
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                ref={emailRef}
+                type="email"
+                placeholder="Email Address"
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Form.Group controlId="password_confirmation">
+              <Form.Label>Repeat Password</Form.Label>
+              <Form.Control
+                ref={passwordConfirmationRef}
+                type="password"
+                placeholder="Repeat Password"
+              />
+            </Form.Group>
+            <Button className="btn btn-block" type="submit">
+              Signup
+            </Button>
+            <p className="message text-center">
+              Already registered? <Link to="/login">Sign In</Link>
+            </p>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
